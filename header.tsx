729b1dad53d4cab3bca2247051ec4ca069abbef5
Basavaraj Hadimani <basuhadimani94@gmail.com>
2018-01-13 16:05:56 +0530

    Add files via upload

---
+++ header.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+class Header extends React.Component {
+   render() {
+      return (
+         <div className="Logo" >
+            <h1><b>Aricent</b></h1>
+         </div>
+      );
+   }
+}
+export default Header;
